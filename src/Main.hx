b163f6e689a78340033411a770a74ec62e9bb1b6
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-01-30 23:22:51 +0100

    Wip

--- src/Main.hx
+++ src/Main.hx
@@ -16,6 +16,9 @@ class Main {
         var surface = SDL.createRGBSurfaceFrom(image.bytes, image.w, image.h, 32, image.w * 4, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
         texture = SDL.createTextureFromSurface(state.renderer, surface);
 
+        SDL.setRenderDrawBlendMode(state.renderer, SDL_BLENDMODE_BLEND);
+        SDL.setTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+
         while(update())
         {
             SDL.delay(4);
