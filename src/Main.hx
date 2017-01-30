e29b0d0002a5650e898cb29120b94c795992f191
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-01-30 23:08:20 +0100

    Almost functional sample

--- src/Main.hx
+++ src/Main.hx
@@ -5,12 +5,16 @@ import sdl.Renderer;
 class Main {
 
     static var state : { window:Window, renderer:Renderer };
-
+    static var texture:sdl.Texture;
     static function main()
     {
-
         SDL.init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-        state = SDL.createWindowAndRenderer(320, 320, SDL_WINDOW_RESIZABLE);
+        state = SDL.createWindowAndRenderer(640, 480, NONE);
+
+        var image = stb.Image.load("data/sdl-logo.png", 4);
+
+        var surface = SDL.createRGBSurfaceFrom(image.bytes, image.w, image.h, 32, image.w * 4, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
+        texture = SDL.createTextureFromSurface(state.renderer, surface);
 
         while(update())
         {
@@ -24,11 +28,12 @@ class Main {
         {
             var e = SDL.pollEvent();
             if(e.type == SDL_QUIT) return false;
-
-            SDL.setRenderDrawColor(state.renderer, 255,255,255,255);
-            SDL.renderClear(state.renderer);
-            SDL.renderPresent(state.renderer);
         }
+
+        SDL.setRenderDrawColor(state.renderer, 128, 128, 128, 255);
+        SDL.renderClear(state.renderer);
+        SDL.renderCopy(state.renderer, texture, null, null);
+        SDL.renderPresent(state.renderer);
 
         return true;
     }
